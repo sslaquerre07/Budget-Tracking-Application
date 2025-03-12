832b8d1206621b4cdc1b36833abe8fbc1ddc5aea
ramos-ahron <ahrondaenielr@gmail.com>
2025-03-11 22:48:06 -0600

    Fetch Budget Title
    Used test email jane.smith

--- frontend/src/components/chatList/ChatList.js
+++ frontend/src/components/chatList/ChatList.js
@@ -1,7 +1,27 @@
 import { Link } from 'react-router-dom';
 import './chatList.css';
+import { useQuery } from '@tanstack/react-query';
 
 function ChatList() {
+    const apiUrl = process.env.REACT_APP_BUDGETS_API;
+    // const apiUrl = "http://localhost:8080"
+
+    const { isPending, error, data } = useQuery({
+        queryKey: ['budgets'],
+        queryFn: () =>
+            fetch(`${apiUrl}/user/budgets`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: "jane.smith@example.com", // !TEST
+                }),
+            }).then((res) => res.json()),
+    });
+
+
+    const budgets = data?.response || [];
 
     return (
         <div className="ChatList">
@@ -10,12 +30,15 @@ function ChatList() {
             <hr />
             <span className="title">RECENT BUDGETS</span>
             <div className="list">
-                <Link to="/">My Budget title</Link>
-                <Link to="/">My Budget title</Link>
-                <Link to="/">My Budget title</Link>
-                <Link to="/">My Budget title</Link>
-                <Link to="/">My Budget title</Link>
-                <Link to="/">My Budget title</Link>
+                {isPending
+                    ? "Loading..."
+                    : error
+                        ? "Something went wrong!"
+                        : budgets.map((budget) => (
+                            <Link to={`/dashboard/budgets/${budget.budgetId}`} key={budget.budgetId}>
+                                {budget.budgetTitle}
+                            </Link>
+                        ))}
             </div>
         </div>
     );
